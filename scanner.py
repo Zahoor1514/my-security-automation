853d14fe1354b701c92e016c6349fd9b9ea700c9
Zahoor1514 <zahoors585@gmail.com>
2026-07-05 11:07:16 +0500

    Testing pipeline failure with dead target

--- scanner.py
+++ scanner.py
@@ -1,9 +1,10 @@
 import urllib.request
-target_url = "https://example.com"
+target_url = "https://this-is-a-fake-and-dead-site-12345.com"
 print(f"[+] Scanning target: {target_url}")
 try:
-response = urllib.request.urlopen(target_url)
-status = response.getcode()
-print(f"[✓] Target is UP. HTTP Status Code: {status}")
+    response = urllib.request.urlopen(target_url)
+    status = response.getcode()
+    print(f"[✓] Target is UP. HTTP Status Code: {status}")
 except Exception as e:
-print(f"[X] Error scanning target: {e}")
+    print(f"[X] Error scanning target: {e}")
+    exit(1)
